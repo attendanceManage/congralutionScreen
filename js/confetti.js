309e0a051c4328b6ebeed20c2b4dc76a932e00bd
Shireen <shireenpeerbuksh2011@gmail.com>
2024-06-24 10:48:42 +0100

    change

---
+++ js/confetti.js
@@ -0,0 +1,139 @@
+//-----------Var Inits--------------
+canvas = document.getElementById("canvas");
+content = document.getElementById("content");
+ctx = canvas.getContext("2d");
+contentx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+cx = ctx.canvas.width ;
+cy = ctx.canvas.height ;
+contentxx = contentx.canvas.width;
+contentxy = contentx.canvas.height;
+
+
+
+let confetti = [];
+const confettiCount = 1000;
+const gravity = 0.5;
+const terminalVelocity = 5;
+const drag = 0.075;
+const colors = [
+{ front: 'red', back: 'darkred' },
+{ front: 'green', back: 'darkgreen' },
+{ front: 'blue', back: 'darkblue' },
+{ front: 'yellow', back: 'darkyellow' },
+{ front: 'orange', back: 'darkorange' },
+{ front: 'pink', back: 'darkpink' },
+{ front: 'gray', back: 'darkpurple' },
+{ front: 'turquoise', back: 'darkturquoise' },
+{ front: 'black', back: 'darkblack' },
+{ front: 'white', back: 'darkwhite' },
+{ front: 'navyblue', back: 'darknavyblue'}
+];
+
+
+//-----------Functions--------------
+resizeCanvas = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  cx = ctx.canvas.width / 2;
+  cy = ctx.canvas.height / 2;
+  contentxx = contentx.canvas.width/2;
+  contentxy = contentx.canvas.height/2;
+  
+  //cy = ctx.canvas.height / 2;
+
+
+};
+
+randomRange = (min, max) => Math.random() * (max - min) + min;
+
+initConfetti = () => {
+  for (let i = 0; i < confettiCount; i++) {
+    confetti.push({
+      color: colors[Math.floor(randomRange(0, colors.length))],
+      dimensions: {
+        x: randomRange(1.30, 10),
+        y: randomRange(10, 10) },
+
+      position: {
+        x: randomRange(0, canvas.width),
+        y: canvas.height - 1 },
+
+      rotation: randomRange(0, 2 * Math.PI),
+      scale: {
+        x: 1,
+        y: 1 },
+
+      velocity: {
+        x: randomRange(-40, 30),
+        y: randomRange(0, -40) } });
+
+
+  }
+};
+
+//---------Render-----------
+render = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "50px Arial";
+  ctx.strokeText("وَتُعِزُ مَن تَشَاء وَتُذِلُ مَن تَشَاء ", 500, 400);
+  confetti.forEach((confetto, index) => {
+    let width = confetto.dimensions.x * confetto.scale.x;
+    let height = confetto.dimensions.y * confetto.scale.y;
+
+    // Move canvas to position and rotate
+    ctx.translate(confetto.position.x, confetto.position.y);
+    ctx.rotate(confetto.rotation);
+
+    // Apply forces to velocity
+    confetto.velocity.x -= confetto.velocity.x * drag;
+    confetto.velocity.y = Math.min(confetto.velocity.y + gravity, terminalVelocity);
+    confetto.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random();
+
+    // Set position
+    confetto.position.x += confetto.velocity.x;
+    confetto.position.y += confetto.velocity.y;
+
+    // Delete confetti when out of frame
+    if (confetto.position.y >= canvas.height) confetti.splice(index, 1);
+
+    // Loop confetto x position
+    if (confetto.position.x > canvas.width) confetto.position.x = 0;
+    if (confetto.position.x < 0) confetto.position.x = canvas.width;
+
+    // Spin confetto by scaling y
+    confetto.scale.y = Math.cos(confetto.position.y * 0.1);
+    ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
+
+    // Draw confetto
+    ctx.fillRect(-width / 2, -height / 2, width, height);
+
+    //contentx.fillRect(-width / 2, -height / 2, width, height);
+   
+
+
+    // Reset transform matrix
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+  });
+
+  // Fire off another round of confetti
+  if (confetti.length <= 10) initConfetti();
+  content.style.display="block";
+  
+  window.requestAnimationFrame(render);
+};
+
+//---------Execution--------
+initConfetti();
+render();
+
+//----------Resize----------
+window.addEventListener('resize', function () {
+  resizeCanvas();
+});
+
+//------------Click------------
+window.addEventListener('click', function () {
+  initConfetti();
+});
